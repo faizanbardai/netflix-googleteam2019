4e5c3181ee217c536b38157bf1362dee3063c56b
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-10-25 11:46:08 +0200

    added Trending Now

---
+++ index.js
@@ -0,0 +1,106 @@
+window.onload = populateMovies
+
+function populateMovies() {
+    getMovies(render);
+};
+
+function getMovies(callback, options) {
+    setTimeout(function () {
+        var movies = MOVIES;
+        if (options && options.category) {
+            movies = movies.filter(function (movie) {
+                return (movie.category === options.category)
+            });
+        };
+        if (options && options.text) {
+            movies = movies.filter(function (movie) {
+                return (movie.title === options.text);
+            })
+        };
+        callback(movies);
+    }, 800);
+};
+
+function render(movies) {
+    var contentDiv = document.getElementById('movie-content');
+    var spinner = document.getElementById('spinner');
+    spinner.classList.remove('d-flex');
+    spinner.classList.add('d-none');
+
+    movies.forEach(function (item) {
+        var card = buildCard(item);
+        contentDiv.appendChild(card);
+    });
+};
+
+function buildCard(data) {
+    // <img class="col-12 col-sm-4 col-lg-3 my-3" src="./movies/1-2.jpg" alt="movie">
+    var img = document.createElement('img');
+    img.className = 'col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-3';
+    img.alt = 'movie';
+    img.src = './img/' + data.img + '.jpg';
+    return img;
+};
+
+var MOVIES = [
+    {
+        title: 'the office - season six',
+        category: 'series',
+        img: '1-1'
+    },
+    {
+        title: 'the terror - season 1',
+        category: 'series',
+        img: '1-2'
+    },
+    {
+        title: 'jack ryan',
+        category: 'movie',
+        img: '1-3'
+    },
+    {
+        title: 'the matrix',
+        category: 'movie',
+        img: '1-4'
+    },
+    {
+        title: 'the man in the high castle',
+        category: 'movie',
+        img: '2-1'
+    },
+    {
+        title: 'desesperate housewives',
+        category: 'movie',
+        img: '2-2'
+    },
+    {
+        title: 'indiana jones and the riders of the lost ark',
+        category: 'movie',
+        img: '2-3'
+    },
+    {
+        title: 'jaws',
+        category: 'movie',
+        img: '2-4'
+    },
+    {
+        title: 'dottor house - season 1',
+        category: 'series',
+        img: '3-1'
+    },
+    {
+        title: 'shrek',
+        category: 'children',
+        img: '3-2'
+    },
+    {
+        title: 'despicable me 2',
+        category: 'children',
+        img: '3-3'
+    },
+    {
+        title: 'vikings',
+        category: 'movie',
+        img: '3-4'
+    }
+];
